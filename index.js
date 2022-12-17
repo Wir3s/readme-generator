fe9edd9fbf776aee375c6d431c6d3b39ed7fc096
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-12-16 20:33:58 -0600

    questions working, data logged

--- index.js
+++ index.js
@@ -1,65 +1,69 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generate = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {   
-        type: 'input',
-        name: 'title',
-        message: 'What is the project title?'
-    },
-    {   
-        type: 'input',
-        name: 'description',
-        message: 'Enter Project Description'
-    },
-    {   
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions'
-    },
-    {   
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information'
-    },
-    {   
-        type: 'input',
-        name: 'contribution',
-        message: 'Enter contribution guidelines'
-    },
-    {   
-        type: 'input',
-        name: 'tests',
-        message: 'Enter test instructions'
-    },
-    {   
-        type: 'checkbox',
-        name: 'license',
-        message: 'Choose a license:'
-    },
-    {   
-        type: 'input',
-        name: 'userName',
-        message: 'Enter your GitHub username'
-    },
-    {   
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address'
-    },    
-    ];
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter Project Description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Choose a license:",
+    choices: ["MIT"],
+  },
+  {
+    type: "input",
+    name: "userName",
+    message: "Enter your GitHub username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
+];
+
+inquirer.prompt(questions).then((data) => console.log(data));
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(README.MD, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-}
+// function writeToFile(fileName, data) {
+//     fs.writeFile(README.MD, JSON.stringify(data, null, '\t'), (err) =>
+//       err ? console.log(err) : console.log('Success!')
+//     );
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
