9d86795fced65c1332bd0c39a535410c47370423
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-12-16 20:05:23 -0600

    Created array

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?'
+    },
+    {   
+        type: 'input',
+        name: 'description',
+        message: 'Enter Project Description'
+    },
+    {   
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions'
+    },
+    {   
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information'
+    },
+    {   
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines'
+    },
+    {   
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions'
+    },
+    {   
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license:'
+    },
+    {   
+        type: 'input',
+        name: 'userName',
+        message: 'Enter your GitHub username'
+    },
+    {   
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address'
+    },    
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(README.MD, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
