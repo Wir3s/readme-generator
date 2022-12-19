44f38b6384950e6db7e7bf20c97ec9df68a69bc9
Wir3s <113479857+Wir3s@users.noreply.github.com>
2022-12-18 19:27:16 -0600

    Badges work, all questions added, more license options added, comments added

--- index.js
+++ index.js
@@ -1,61 +1,61 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generate = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
   {
     type: "input",
     name: "title",
     message: "What is the project title?",
   },
-  // {
-  //   type: "input",
-  //   name: "description",
-  //   message: "Enter Project Description",
-  // },
-  // {
-  //   type: "input",
-  //   name: "installation",
-  //   message: "Enter installation instructions",
-  // },
-  // {
-  //   type: "input",
-  //   name: "usage",
-  //   message: "Enter usage information",
-  // },
-  // {
-  //   type: "input",
-  //   name: "contribution",
-  //   message: "Enter contribution guidelines",
-  // },
-  // {
-  //   type: "input",
-  //   name: "tests",
-  //   message: "Enter test instructions",
-  // },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter project description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Enter contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions",
+  },
   {
     type: "checkbox",
     name: "license",
     message: "Choose a license:",
-    choices: ['MIT', 'Mozilla'],
+    choices: ['MIT', 'Mozilla', 'Apache', 'GPLv2'],
   },
-  // {
-  //   type: "input",
-  //   name: "userName",
-  //   message: "Enter your GitHub username",
-  // },
-  // {
-  //   type: "input",
-  //   name: "email",
-  //   message: "Enter your email address",
-  // },
+  {
+    type: "input",
+    name: "userName",
+    message: "Enter your GitHub username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+  },
 ];
 
 inquirer.prompt(questions).then((data) => writeToFile(data));
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(data) {
   console.log(data);
   fs.writeFile("./utils/README.MD", generate(data), (err) =>
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,14 @@ function renderLicenseBadge(license) {
       licenseBadge =
         "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
       return licenseBadge;
-    default:
+    case "Apache":
+      licenseBadge =
+        "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      return licenseBadge;
+    case "GPLv2":
+      licenseBadge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
+      return licenseBadge
+      default:
       licenseBadge = "";
   }
 }
@@ -30,7 +37,8 @@ function renderLicenseBadge(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   console.log(data);
-  return `# ${data.title}
+  return `${renderLicenseBadge(data.license)}
+  # ${data.title}
 
 ## Description
   ${data.description}
@@ -40,7 +48,7 @@ function generateMarkdown(data) {
   [Usage](#usage)  
   [Contribution](#contribution)  
   [License](#license)  
-  [Tests](#tests)
+  [Tests](#tests)  
   [Questions](#questions)
 
 ## Installation
@@ -55,10 +63,13 @@ function generateMarkdown(data) {
 ## Tests
 
 ## Questions
+  ${data.userName}  
+  [GitHub Profile](https://github.com/${data.userName})  
+  Email me at ${data.email} with any additional questions.
 
 ## License
   ${data.license}
-  ${renderLicenseBadge(data.license)}
+  
 `;
 }
 
